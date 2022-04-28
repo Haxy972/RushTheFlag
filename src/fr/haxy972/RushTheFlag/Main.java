8a93cb30604744984f9bae6a843ae37c88bbc07b
Haxy <Haxy@outlook.fr>
2022-04-28 15:48:47 +0200

    Project Init

---
+++ src/fr/haxy972/RushTheFlag/Main.java
@@ -0,0 +1,19 @@
+package fr.haxy972.RushTheFlag;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    Main INSTANCE;
+
+    @Override
+    public void onEnable() {
+        this.INSTANCE = this;
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
